3ccc445b767df1a7ad59d12b88b1f8b22ecbfe8f
Tanmay-262 <104974870+Tanmay-262@users.noreply.github.com>
2024-05-28 17:00:08 +0530

    Update Source code.py

--- Source code.py
+++ Source code.py
@@ -1,1 +1,7 @@
-
+n=int(input("Enter the number of terms needed in Fibonacci Series:\n"))
+fib=[]
+a,b=0,1
+for i in range(n):
+    fib.append(a)
+    a,b=b,a+b
+print("The required Fibonacci series is:-\n",fib)
